536b1372cac2fdabe749741ec3a14f8cf586b59d
nandamatost <fefateodoroteodoro@gmail.com>
2022-11-11 18:04:34 -0300

    commit inicial

---
+++ src/screens/Login/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, TextInput, Image, Text, TouchableOpacity} from 'react-native';
+import style from './styles';
+import gato from './../../assets/Imagem-Gato-PNG.png';
+
+const Login = () => {
+  return (
+  <View style={style.container}>
+    <Image source={gato} style={style.gato}/>
+        <TextInput style={style.TextInput}/>
+        <TextInput style={style.TextInput}/>
+
+        <TouchableOpacity>
+            <Text style={style.text}>
+                Entrar
+            </Text>
+        </TouchableOpacity>
+
+        <Text style={style.text}>Esqueci minha senha</Text>
+  </View>
+  )
+};
+
+export default Login;
---
+++ src/screens/Login/styles.js
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f69',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "#fff",
+    fontSize: 20
+
+},
+TextInput: {
+    backgroundColor: "#FFFF",
+    width: 140,
+    height: 40,
+    justifyContent: "center"
+
+},
+gato: {
+    width: 140,
+    height: 140,
+    justifyContent: "center"
+}
+});
+export default style;
